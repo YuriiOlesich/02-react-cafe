1580c10e78c7f9d1631498fa1191bbf211b2a4d4
Yurii <olesich2007@gmail.com>
2026-04-28 12:13:04 +0300

    do homework-2 for react

---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,40 @@
+import css from './VoteOptions.module.css';
+
+type VoteType = 'good' | 'neutral' | 'bad';
+
+interface VoteOptionsProps {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteOptionsProps) {
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={() => onVote('good')}>
+        Good
+      </button>
+
+      <button className={css.button} onClick={() => onVote('neutral')}>
+        Neutral
+      </button>
+
+      <button className={css.button} onClick={() => onVote('bad')}>
+        Bad
+      </button>
+
+      {canReset && (
+        <button
+          className={`${css.button} ${css.reset}`}
+          onClick={onReset}
+        >
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
